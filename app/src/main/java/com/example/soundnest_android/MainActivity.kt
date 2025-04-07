7a0297f999f2e038efeacc092a16b5c944d23140
unaay20 <junml.kimmich@gmail.com>
2025-04-06 19:08:54 -0600

    Proof commit

--- app/src/main/java/com/example/soundnest_android/MainActivity.kt
+++ app/src/main/java/com/example/soundnest_android/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        //hola, commit prueba
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
